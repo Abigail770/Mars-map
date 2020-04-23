7ff42c356f1639b14ce18107f843588bf305036f
Abby Gleason <54085652+Abigail770@users.noreply.github.com>
2020-04-23 18:12:33 -0500

    Create detail panel and add json files

--- js/story.js
+++ js/story.js
@@ -3,7 +3,7 @@
 // the basemap selector, the rover selection buttons, etc.
 var storymode = false;
 
-function toggle_story_mode(map) {
+function toggle_story_mode(map, obj) {
     if (storymode) {
         console.log('returning to main map');
         // Code to return main map to normal
@@ -16,9 +16,8 @@ function toggle_story_mode(map) {
         
         //// Show basemap selector
         
-        
         //// Remove details panel
-        
+        $('#panel').css({'display':'none'});
         
         //// Reset map zoom bounds to default
         map.setMinZoom(2);
@@ -28,12 +27,40 @@ function toggle_story_mode(map) {
         
     } else {
         console.log('setting up storymode');
+
         // Code to transition to story mode
-        
+        $('.card-title').html(obj[0].Title);
+        $('.card-text').html(obj[0].Content);
+
+        var current = 0;
+
+        function loop(){
+            var titleOutput = obj[current].Title
+            var contentOutput = obj[current].Content
+            $('.card-title').html(titleOutput);
+            $('.card-text').html(contentOutput);
+        }
+        function next(){
+            current++;
+            loop();
+        }
+        function prev(){
+           current--;
+           loop();
+        }
+
+        $('#next-btn').on('click', function () {
+             next();
+        })
+
+        $('#back-btn').on('click', function () {
+            prev();
+        })
         //// Hide basemap selector
-        
+
         //// Show details panel
-        
+        $('#panel').css({'display':'block'});
+
         
         storymode = true;
     }
